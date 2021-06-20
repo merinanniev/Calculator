0bccb9a76fcdfd2ee5c9b42002889a4456bbc328
merinanniev <merin.annie@aspirenxt.com>
2021-06-20 17:12:42 +0530

    Additional Changes

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ def divide(x, y):
    return x / y  
 def modulus(x,y):
    return x % y
-print("Select operation.")  
+print("Select an operation")  
 print("1.Add")  
 print("2.Subtract")  
 print("3.Multiply")  
