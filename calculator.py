0ffe23942f1c0c3556ccbac36df111c07a273c0f
merinanniev <merin.annie@aspirenxt.com>
2021-06-20 13:31:12 +0530

    Added functions

--- calculator.py
+++ calculator.py
@@ -22,7 +22,13 @@ if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))  
   
 elif choice == '2':  
-   print(num1,"-",num2,"=", subtract(num1,num2))  
+   print(num1,"-",num2,"=", subtract(num1,num2)) 
+
+ elif choice == '3':  
+   print(num1,"*",num2,"=", multiply(num1,num2)) 
+   
+elif choice == '4':  
+   print(num1,"/",num2,"=", divide(num1,num2)) 
  
 else:  
    print("Invalid input")  
