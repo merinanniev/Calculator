b7a963668ac4ddef3346a45c72d94caf5ba899a6
merinanniev <merin.annie@aspirenxt.com>
2021-06-20 16:44:21 +0530

    Additional Changes

--- calculator.py
+++ calculator.py
@@ -17,8 +17,8 @@ print("5.Modulus")
   
 choice = input("Enter choice(1/2/3/4):")  
   
-num1 = int(input("Enter first number: "))  
-num2 = int(input("Enter second number: "))  
+num1 = int(input("Enter 1st number: "))  
+num2 = int(input("Enter 2nd number: "))  
   
 if choice == '1':  
    print(num1,"+",num2,"=", add(num1,num2))  
